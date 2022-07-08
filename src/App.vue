10307eaedd9f185c0e1272fd193dae960c0689e0
Ponja94 <henrique.danielb@gmail.com>
2022-07-08 16:42:02 -0300

    3rd commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <OutroComponente />
     <p>O Dado de App é: {{ testando }}</p>
     <LifeCycle />
+    <Pessoa />
   </div>
 </template>
 
@@ -13,13 +14,15 @@
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import OutroComponente from './components/OutroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
 
   export default{
     name:'App',
     components: {
       PrimeiroComponente,
       OutroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoa,
     },
     data(){
       return{
---
+++ src/components/form/Submit.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input type="text" value="Enviar">
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'Submit'
+}
+
+</script>
